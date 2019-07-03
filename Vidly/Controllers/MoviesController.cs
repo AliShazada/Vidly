c370e1e2aed16b81f2597d9a2dbacae4f7bbe78f
Ali Ahmad Shazada <aliahmadshahzada@outlook.com>
2019-07-03 22:41:08 +1000

    .

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            return View(movie);
+        }
+
+        public ActionResult Edit(int movieId)
+        {
+            return Content("id=" + movieId);
+        }
+
+        // Optional parameter 
+
+
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+
+        //    if (string.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+
+        //    return Content(String.Format("pageindex={0}&sortBy={1}", pageIndex, sortBy));
+                                 
+        //}
+
+
+
+    }
+}
